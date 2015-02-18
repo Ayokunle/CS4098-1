d9eccc5a5ed9100a5483f1ffb4ffdab5a2910071
Eoin Flanagan <outterest@hotmail.com>
2015-02-18 19:45:09 +0000

    Created ajax call for the 'start' process action Added angular data bindings

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 var app = angular.module('myApp', []);
-var app = angular.module('popupApp', []);
+var popupapp = angular.module('popupApp', []);
+
+var hello_world = "hello"
+function test () {
+	hello_world = "hi world"
+}
+
+function actionController($scope) {
+	$scope.actionName = "Action1";
+	$scope.actionState = "None";
+
+	$scope.start = 
+		function process_start() {
+			xmlhttp=new XMLHttpRequest();
+			console.log("Starting " + $scope.actionName);
+			xmlhttp.onreadystatechange=function() {
+		  		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+		  			$scope.actionState = "Started";
+				}
+				else {
+					$scope.actionState = "Failed request";
+				}
+			}
+
+			xmlhttp.open("GET","backend/kernel_request.py",true);
+			xmlhttp.send();
+		};
+}
