1281e7294e59ea62eb031725a700892bba2c8c8c
Ayokunle <adeosua@tcd.ie>
2015-02-18 23:53:27 +0000

    update xml parser

--- backend/xml_parser.py
+++ backend/xml_parser.py
@@ -1,21 +1,45 @@
+#!/usr/bin/python 
+
+import os
+import sys
+import json
+import socket             
+import subprocess
 try:
     from lxml import etree
 except ImportError:
     import xml.etree.ElementTree as etree
 
-tree = etree.parse('process_table.xml')  
 
-for process in tree.iter("process"):
-	print("%s - %s" % (process.tag, process.attrib))
-	for action in process.iter("action"):
-		print(" %s - %s" % (action.tag, action.attrib))
-		
-		for script in action.iter("script"):
-			print("  %s - %s" % (script.tag, script.text))
-		
-		for req_resource in action.iter("req_resource"):
-			print("  %s - %s" % (req_resource.tag, req_resource.text))
-		
-		for prov_resource in action.iter("prov_resource"):
-			print("  %s - %s" % (prov_resource.tag, prov_resource.text))
+MAX_CONNECTION_REQUEST_QUEUE = 5
+
+server = socket.socket() 
+host = socket.gethostname()
+port = sys.argv[1]        
+server.bind((host, int(port)))
+
+server.listen(MAX_CONNECTION_REQUEST_QUEUE)   
+
+while True:
+	client, address = server.accept()
+	request = client.recv(1024)
+	request = json.loads(request)
+	
+	if request['event'] == "GET_TABLE":
+
+		tree = etree.parse('process_table.xml')  
+
+		for process in tree.iter("process"):
+			print("%s - %s" % (process.tag, process.attrib))
+			for action in process.iter("action"):
+				print(" %s - %s" % (action.tag, action.attrib))
+				
+				for script in action.iter("script"):
+					print("  %s - %s" % (script.tag, script.text))
+				
+				for req_resource in action.iter("req_resource"):
+					print("  %s - %s" % (req_resource.tag, req_resource.text))
+				
+				for prov_resource in action.iter("prov_resource"):
+					print("  %s - %s" % (prov_resource.tag, prov_resource.text))
 
