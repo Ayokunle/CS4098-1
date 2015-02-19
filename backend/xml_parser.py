5e727f54bb2cd67db3fcf18bb96cd855ca6f5ee1
Ayokunle <adeosua@tcd.ie>
2015-02-19 00:01:34 +0000

    update xml parser

--- backend/xml_parser.py
+++ backend/xml_parser.py
@@ -26,20 +26,37 @@ while True:
 	request = json.loads(request)
 	
 	if request['event'] == "GET_TABLE":
+		data = {}
+		data['key'] = 'value'
+		json_data = json.dumps(data)
 
 		tree = etree.parse('process_table.xml')  
 
 		for process in tree.iter("process"):
 			print("%s - %s" % (process.tag, process.attrib))
+			data['process.tag'] = process.tag
+			data['process.attrib'] = process.attrib
+
 			for action in process.iter("action"):
 				print(" %s - %s" % (action.tag, action.attrib))
+				data['action.tag'] = action.tag
+				data['action.attrib'] = action.attrib
 				
 				for script in action.iter("script"):
 					print("  %s - %s" % (script.tag, script.text))
+					data['script.tag'] = script.tag
+					data['script.text'] = script.text
 				
 				for req_resource in action.iter("req_resource"):
 					print("  %s - %s" % (req_resource.tag, req_resource.text))
-				
+					data['req_resource.tag'] = req_resource.tag
+					data['req_resource.text'] = req_resource.text
+
 				for prov_resource in action.iter("prov_resource"):
 					print("  %s - %s" % (prov_resource.tag, prov_resource.text))
+					data['prov_resource.tag'] = prov_resource.tag
+					data['prov_resource.text'] = prov_resource.text
+		
+	client.send(error)
+	client.close()
 
