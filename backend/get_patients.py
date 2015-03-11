9eac541fa960ee10b2b128b19a41a3a691d1d92f
Ayokunle Adeosun <adeosua@tcd.ie>
2015-03-11 20:56:38 +0000

    Update get_patients.py

--- backend/get_patients.py
+++ backend/get_patients.py
@@ -1,9 +1,20 @@
+#!/usr/bin/python
+import os
+import re
+import sys
+import json
+import urllib
+import socket             
 import subprocess
+import cgi, cgitb 
+
+
 cmd = "mysqldump --xml -u root openemr patient_data"
 
 xml = subprocess.check_output(cmd, shell=True)
-#print xml
+print "Content-Type: text/html\n"
+print xml
 
-file = open("patient_data.xml", "w")
+file = open("patient_data.xml", "w+")
 file.write(xml)
 file.close()
