0216e7646a9fcdc5b5313de03d4bc3d49757353f
Calturk <jason.callaghan1@gmail.com>
2015-03-11 17:00:41 +0000

    Action popup working with angular

--- js/popup.js
+++ js/popup.js
@@ -5,3 +5,7 @@ $(document).ready(function() {
         $(this).parent().children(".expanded, .collapsed").toggle();
    	});
 });
+
+//var actionList = {State:"Ready", required_resources:{patient_record:"001671458.hl7"}, provided_resources:{patient_symptoms:"001671458.spt"}};
+
+//var actionList = {State:"Ready", required_resources:"001671458.hl7", provided_resources:{patient_symptoms:"001671458.spt"}};
