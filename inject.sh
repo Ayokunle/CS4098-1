64f79d1504347dccf27f5593e8cfe12793635806
Michael Rooney <m.rooney93@gmail.com>
2015-03-17 17:58:38 -0400

    Fixing injection, adding install scripts

--- inject.sh
+++ inject.sh
@@ -5,4 +5,4 @@ PROJECT=Shcyup
 INJECT_AFTER_LINE="<?php echo htmlspecialchars(xl('Issues'),ENT_NOQUOTES); ?></a>"
 INJECTED_LINE="\n|\n<a href=\"/Shcyup/app/pathways/pathways.html?patient_id=<?php echo \$pid;?>\" class='iframe large_modal' onclick='top.restoreSession()'>\n<?php echo htmlspecialchars(xl('Pathway Support'),ENT_NOQUOTES); ?></a>"
 
-sed -i 's:${INJECTED_AFTER_LINE}:&${INJECTED_LINE}:g' $INJECTION_FILE
+sed -i "s:${INJECT_AFTER_LINE}:&${INJECTED_LINE}:g" $INJECTION_FILE
---
+++ install64.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+sudo apt-get install make
+sudo apt-get install check
+sudo apt-get install expect
+sudo apt-get install libxml2
+sudo apt-get install tcl-dev
+sudo apt-get install libreadline-dev
+sudo apt-get install libn32curses5-dev
+sudo apt-get install apache2
+sudo apt-get install python3
+sudo apt-get install bison
+sudo apt-get install flex
+sudo apt-get install python-pip
+sudo apt-get install dos2unix
+sudo pip install https://github.com/hay/xml2json/zipball/master
+sudo make install
+git clone https://github.com/jnoll/peos
+cd peos
+make
