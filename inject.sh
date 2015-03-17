9e3323c0211a12de03bea822a20b0f2e9830b178
Eoin Flanagan <outterest@hotmail.com>
2015-03-17 15:17:00 +0000

    Implemented injection into patient demographics screen

--- inject.sh
+++ inject.sh
@@ -1,12 +1,8 @@
 OPENEMR_DIR=/var/www/openemr
-INJECTION_FILE=${OPENEMR_DIR}/interface/main/main_title.php
+INJECTION_FILE=${OPENEMR_DIR}/interface/patient_file/summary/demographics.php
 PROJECT=Shcyup
 
-#Add 
-#
-#<a href="Shcyup/app/pathways/pathways.html" class='iframe large_modal' onclick='top.restoreSession()'>
-#<?php echo htmlspecialchars(xl('Pathway Support'),ENT_NOQUOTES); ?></a>
-#
-#to main/interface/patient_file/summary/demographics.php
+INJECT_AFTER_LINE="<?php echo htmlspecialchars(xl('Issues'),ENT_NOQUOTES); ?></a>"
+INJECTED_LINE="\n|\n<a href=\"/Shcyup/app/pathways/pathways.html?patient_id=<?php echo \$pid;?>\" class='iframe large_modal' onclick='top.restoreSession()'>\n<?php echo htmlspecialchars(xl('Pathway Support'),ENT_NOQUOTES); ?></a>"
 
-sed 
+sed -i 's:${INJECTED_AFTER_LINE}:&${INJECTED_LINE}:g' $INJECTION_FILE
