5ca5300af300a826ebcb2b1729a51c1dcfc5f10e
thysol <henrik.bruesecke@gmail.com>
2015-04-08 18:53:06 +0100

    Connected peos_notify

--- backend/peos_notify.py
+++ backend/peos_notify.py
@@ -12,24 +12,18 @@ from os import listdir
 from os.path import isfile, join
 
 #http://178.62.51.54:13930/event=CREATE&login_name=henrik&pathway_name=test_commit.pml
-EXECUTION_PATH = "CS4098/peos/os/kernel/"
-MODEL_PATH = "../../models/"
-XML_PARSER_PATH = "CS4098/backend/"
-MAX_CONNECTION_REQUEST_QUEUE = 5
-
-
-#Error constants
-ERROR_USER_NOT_EXIST = 1
-ERROR_SCRIPT_FAIL = 2
-
-
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-os.chdir(EXECUTION_PATH)
-
-request = cgi.FieldStorage()
 
 def peos_notify(patient_id):
+    EXECUTION_PATH = "CS4098/peos/os/kernel/"
+
+    #Error constants
+    ERROR_USER_NOT_EXIST = 1
+    ERROR_SCRIPT_FAIL = 2
+    
 	try:
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        os.chdir(EXECUTION_PATH)
+
 	    process = subprocess.Popen(["./peos", "-l", patient_id, "-u" ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	    output, error = process.communicate()
 
