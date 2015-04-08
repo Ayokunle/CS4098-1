d41e793c9ec83700d4f81d91b9a7cb026e1b82df
Mikey Rooney <m.rooney93@gmail.com>
2015-04-08 20:07:28 +0100

    Update DELETE_PROCESS_test.py

--- test/DELETE_PROCESS_test.py
+++ test/DELETE_PROCESS_test.py
@@ -12,14 +12,14 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 os.chdir(EXECUTION_PATH)
 
 #Creating a process
-response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=CREATE&login_name=10001&pathway_name=Dementia_management.pml").read()
+response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=CREATE&login_name=27&pathway_name=Dementia_management.pml").read()
 
 #10000ing if CGI script will return Json with process table
-response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=GETLIST&login_name=10001").read()
+response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=GETLIST&login_name=27").read()
 response = str(response)
 
 #Deleting process...
-response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=DELETE&login_name=10001&process_id=0").read()
+response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=DELETE&login_name=27&process_id=0").read()
 
 if ("Dementia_management" in response):
     sys.exit(1)
