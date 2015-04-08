2975960cadd68b9cf225981590a26cedcbbf0d0d
Mikey Rooney <m.rooney93@gmail.com>
2015-04-08 16:43:12 +0100

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -4,27 +4,47 @@ echo "Running Shcyup automated tests"
 error=0
 
 if [ -f "../peos/os/kernel/peos" ]; then
-	echo "OK: Peos executable exists"
+        echo "OK: Peos executable exists"
 else
-	echo "ERROR: Peos executable does not exist"
-	error=1
+        echo "ERROR: Peos executable does not exist"
+        error=1
 fi
 
 if [ -f "/usr/lib/cgi-bin/kernel_request.py" ]; then
-	echo "OK: Kernel request script exists"
+        echo "OK: Kernel request script exists"
 else
-	echo "ERROR: Kernel request script does not exist"
-	error=1
+        echo "ERROR: Kernel request script does not exist"
+        error=1
 fi
 
 python3 contact_backend_test.py
 rc=$?
 
 if [[ $rc != 0 ]]; then
-        echo "FAIL: Could not communicate with CGI script"
+        echo "ERROR: Could not communicate with CGI script"
         error=1
 else
         echo "OK: Can communicate with CGI script"
+fi
+
+python3 GETLIST_test.py
+rc=$?
+
+if [[ $rc != 0 ]]; then
+        echo "ERROR: Could not create a process"
+        error=1
+else
+        echo "OK: Process was created"
+fi
+
+python3 CREATE_PROCESS_test.py
+rc=$?
+
+if [[ $rc != 0 ]]; then
+        echo "ERROR: Script was not able to create a process"
+        error=1
+else
+        echo "OK: Script was able to create a process"
 fi
 
 if [ -d "/var/www/openemr" ]; then
@@ -35,9 +55,9 @@ else
 fi
 
 if [ "$error" -eq 1 ]; then
-	echo "FAIL: Errors were encountered in testing"
+        echo "FAIL: Errors were encountered in testing"
 else
-	echo "SUCCESS: All tests passed"
+        echo "SUCCESS: All tests passed"
 fi
 
 echo "******"
