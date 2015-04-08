07706d15c4e0a546ddadae91de90af40ef09e0c8
thysol <henrik.bruesecke@gmail.com>
2015-04-08 18:00:09 +0100

    Change to prevent tests from interfering

--- test/DELETE_PROCESS_test.py
+++ test/DELETE_PROCESS_test.py
@@ -11,18 +11,16 @@ MODEL_PATH = "../../models/"
 os.chdir(EXECUTION_PATH)
 
 #Creating a process
-response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=CREATE&login_name=10000&pathway_name=Dementia_management.pml").read()
+response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=CREATE&login_name=10001&pathway_name=Dementia_management.pml").read()
 
 #10000ing if CGI script will return Json with process table
-response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=GETLIST&login_name=10000").read()
+response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=GETLIST&login_name=10001").read()
 response = str(response)
 
 #Deleting process...
-response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=DELETE&login_name=10000&process_id=0").read()
+response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=DELETE&login_name=10001&process_id=0").read()
 
 if ("Dementia_management" in response):
-    print("10000 failed")
     sys.exit(1)
 else:
-    print("10000 passed")
     sys.exit(0)
