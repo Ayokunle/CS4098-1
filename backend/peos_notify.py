5ac62a57234ce2b057f029fe1a8de06eab8a4e9c
thysol <henrik.bruesecke@gmail.com>
2015-04-08 19:26:45 +0100

    Fixed indents

--- backend/peos_notify.py
+++ backend/peos_notify.py
@@ -20,16 +20,16 @@ def peos_notify(patient_id):
     ERROR_USER_NOT_EXIST = 1
     ERROR_SCRIPT_FAIL = 2
     
-	try:
+    try:
         os.chdir(os.path.dirname(os.path.realpath(__file__)))
         os.chdir(EXECUTION_PATH)
 
-	    process = subprocess.Popen(["./peos", "-l", patient_id, "-u" ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	    output, error = process.communicate()
+	process = subprocess.Popen(["./peos", "-l", patient_id, "-u" ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	output, error = process.communicate()
 
     	jsonreply = {"status" : "success", "output" : {"o" : output, "e" : error}}
-	except Exception as ex:
-		jsonreply = {"error": "%s\n%s" % (type(ex), ex), "error_code" : ERROR_SCRIPT_FAIL}
+    except Exception as ex:
+	jsonreply = {"error": "%s\n%s" % (type(ex), ex), "error_code" : ERROR_SCRIPT_FAIL}
 
 	print json.dumps(jsonreply)
 
