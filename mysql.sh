fb5a44e6a2c2ac628c4cc07ad968f22963aca2e5
Ayokunle Adeosun <adeosua@tcd.ie>
2015-04-08 13:58:56 +0100

    Create mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,11 @@
+while true; do
+    read -p "Please enter your mySQL password" password
+    result=$( (mysql -u root -p$password -s -e "use mysql; update user set password=PASSWORD('') where User='root'; flush privileges; \q") 2>&1 > /dev/null )
+    
+    if [[ $result == *"ERROR"* ]]
+	then
+  		echo "Wrong password, please try again."
+	else
+		break;
+	fi
+done
