4fc49883d50f5b1d6cf345bc627818791faffa28
Mikey Rooney <m.rooney93@gmail.com>
2015-04-10 18:21:51 +0100

    Update GETLIST_test.py

--- test/GETLIST_test.py
+++ test/GETLIST_test.py
@@ -12,15 +12,15 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 os.chdir(EXECUTION_PATH)
 
 #Creating a process
-process = subprocess.Popen(["./peos", "-l", "27", "-c", MODEL_PATH + "Dementia_management.pml"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process = subprocess.Popen(["./peos", "-l", "41", "-c", MODEL_PATH + "Dementia_management.pml"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 output, error = process.communicate()
 
 #27ing if CGI script will return Json with process table
-response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=GETLIST&login_name=27").read()
+response = urllib.request.urlopen("http://127.0.0.1/cgi-bin/kernel_request.py/?event=GETLIST&login_name=41").read()
 response = str(response)
 
 #Clean up (Adding a process at random may have side effects....)
-process = subprocess.Popen(["./peos", "-l", "27", "-d", "0"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process = subprocess.Popen(["./peos", "-l", "41", "-d", "0"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 if ("Dementia_management" in response):
     sys.exit(0)
