fb1a59a79e965048ab2460412ccafe7be7c3810e
Mikey Rooney <m.rooney93@gmail.com>
2015-04-10 22:59:26 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,22 +1,19 @@
 while true; do
     result=$( (mysql -u root -s -e "use mysql; update user set password=PASSWORD('') where User='root'; flush privileges; \q") 2>&1 > /dev/null )
-    
-    if [[ $result == *"ERROR"* ]]
-	then
-		echo $result
-		read -p "Please enter your mySQL password: " password
-		result=$( (mysql -u root -p$password -s -e "use mysql; update user set password=PASSWORD('') where User='root'; flush privileges; \q") 2>&1 > /dev/null )
-  		
-  		if [[ $result == *"ERROR"* ]]
-		then
-			echo "Wrong password, please try again."
-		else
-			break;
-		fi
-		
-	else
-		break;
-	fi
-done
 
-mysql -D openemr -u root < preamble.sql || true
+    if [[ $result == *"ERROR"* ]]
+        then
+                read -p "Please enter your mySQL password: " password
+                result=$( (mysql -u root -p$password -s -e "use mysql; update user set password=PASSWORD('') where User='root'; flush privileges; \q") 2>&1 > /dev/null )
+
+                if [[ $result == *"ERROR"* ]]
+                then
+                        echo "Wrong password, please try again."
+                else
+                        break;
+                fi
+
+        else
+                break;
+        fi
+done
